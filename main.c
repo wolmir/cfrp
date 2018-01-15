eaeaa06728e52eb67ceb7131b5ce77510f60abda
Wolmir Nemitz Benito <wolmir.nemitz@gmail.com>
2018-01-14 22:09:44 -0200

    Add one more test of the tap op

--- main.c
+++ main.c
@@ -3,23 +3,27 @@
 #include "c_frp.h"
 
 void *print_values(void *value);
+void *print_mult_values(void *value);
 
 int main()
 {
   EVENT_STREAM *test_stream;
-  EVENT_STREAM *listener;
   int numa = 3;
   int numb = 5;
   int numc = 8;
+  int numd = 13;
 
   test_stream = cfrp_create_event_stream();
 
   cfrp_stream_send(test_stream, &numa);
   cfrp_stream_send(test_stream, &numb);
 
-  listener = cfrp_stream_tap(test_stream, print_values);
+  cfrp_stream_tap(test_stream, print_values);
+
+  cfrp_stream_tap(test_stream, print_mult_values);
 
   cfrp_stream_send(test_stream, &numc);
+  cfrp_stream_send(test_stream, &numd);
 
   return 0;
 }
@@ -27,6 +31,13 @@ int main()
 void *print_values(void *value)
 {
   printf("Value: %d\n", *((int*)value));
+
+  return NULL;
+}
+
+void *print_mult_values(void *value)
+{
+  printf("Value: %d\n", *((int*)value) * 2);
 
   return NULL;
 }
